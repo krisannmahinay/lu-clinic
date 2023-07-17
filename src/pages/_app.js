cada1c1466e213747b50d3fb013da86c2cfd6e1d
cortejojicoy <jicoy.cortejo@gmail.com>
2023-07-18 00:53:03 +0800

    update main app using persist redux to manage state

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,20 @@
 import 'tailwindcss/tailwind.css'
 import { useRouter } from 'next/router'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
+import { useDispatch, useSelector } from 'react-redux'
+import { makeStore } from '../store/store'
+import { useEffect } from 'react'
 
 export default function App ({Component, pageProps}) {
-    const router = useRouter()
-    return <Component key={router.asPath}  {...pageProps}/>
+    const { store, persistor } = makeStore()
+
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Component {...pageProps}/>
+            </PersistGate>
+        </Provider>
+    )
 }
-
-
-// export default App
+ 
