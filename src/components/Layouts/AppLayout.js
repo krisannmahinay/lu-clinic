da6b44767ecf45d0c4e2cda371abc3abed57ff2e
cortejojicoy <jicoy.cortejo@gmail.com>
2023-07-18 00:51:20 +0800

    implemented redux and alert error

--- src/components/Layouts/AppLayout.js
+++ src/components/Layouts/AppLayout.js
@@ -1,44 +1,34 @@
 import { useEffect, useState } from 'react'
 import Navigation from '../../components/Layouts/Navigation'
-import { useAuth, GetUsers } from '../../hooks/auth'
 import { useRouter } from 'next/router'
-import AuthMiddleware from '../../lib/auth'
-import Cookies from 'js-cookie';
-import { redirect } from 'next/dist/server/api-utils'
+import { useDispatch, useSelector } from 'react-redux'
+import { setCredentials } from '../../store/reducers/authSlice'
+import { useGetUserDetailsQuery } from '../../service/authService'
+import store from '../../store/store'
 import SystemError from '../SystemError'
 
 const AppLayout = ({ header, children }) => {
     const router = useRouter()
-    // const { user } = useAuth({ middleware: 'auth', })
-    const { user } = GetUsers({path: '/api/user'})
-    const restrictedUrl = ('/dashboard')
-    const loggedIn = Cookies.get('loggedIn');
-    const test = AuthMiddleware(loggedIn, restrictedUrl)
-    console.log(loggedIn)
-    // useEffect(() => {
-    //     !user && router.push('/login')
-    // }, [])
-    // const [navMenu, setNavMenu] = useState([])
-    
-    // if(user) {
-    //     const menu = JSON.parse(user.menus)
-    //     // console.log(menu.Appointments.show)
-    //     // setNavMenu(menu)
-    // }
-    // let menus = Object.keys(user.menus)
-    
-    // if(user) {
-    //     const menu = JSON.parse(user.menus)
-    //     console.log(menu)
-    // }
+    const dispatch = useDispatch()
+    const { isLoggedIn, loading } = useSelector((state) => state.auth)
+    const { data } = useGetUserDetailsQuery()
 
-    return (
-        <>
-            <div className="min-h-screen bg-gray-100">
-                <Navigation user={user} child={children} />
-            </div>  
-        </>
-    )
+    // if(data) {
+    //     dispatch(setCredentials(data)) 
+    // }
+    useEffect(() => {
+        if(data) {
+            dispatch(setCredentials(data)) 
+        }
+    },[])
+
+    console.log(loading)
+    if(!isLoggedIn) {
+        return <SystemError />
+    }
+
+    return <Navigation user={data} children={children} />
 }
 
 export default AppLayout
+
