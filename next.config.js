7d1b5c1d81ae3af9a419c6acb181bb194e7fe235
cortejojicoy <jicoy.cortejo@gmail.com>
2023-07-28 14:22:38 +0800

    update configuration for pwa

--- next.config.js
+++ next.config.js
@@ -8,5 +8,13 @@ module.exports = {
     env: {
         API_URL: 'http://localhost/api',
         CSRF: 'http://localhost/sanctum/csrf-cookie'
+    },
+
+    async rewrites() {
+        const pages = require('./src/pages/_document').getPages()
+        return pages.map((page) => ({
+            source: `/${page}`,
+            destination: `/${page}`
+        }))
     }
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,19 @@
 import Document, { Html, Head, Main, NextScript }  from "next/document";
+import fs from 'fs'
+import path from "path";
 
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx)
         return { ...initialProps }
+    }
+
+    static async getPages() {
+        const pageDir = path.join(process.cwd(), 'pages')
+        const files = fs.readdirSync(pageDir)
+        return files
+            .filter((file) => file !== '_document.js' && file !== '_app.js' && !file.endsWith('.test.js'))
+            .map((file) => file.replace(/\.js$/, ''))
     }
 
     render() {
