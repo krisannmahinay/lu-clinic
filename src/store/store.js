754d4a99ad26199c7543984cd073f70178bda16f
cortejojicoy <jicoy.cortejo@gmail.com>
2023-08-01 11:30:50 +0800

    merge latest to main

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-<<<<<<< HEAD
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import authReducer from './reducers/authSlice'
@@ -25,22 +24,3 @@ export const makeStore = () => {
 
   return { store, persistor }
 }
-=======
-import authReducer from './reducers/authSlice';
-import { authApi } from '../service/authService'
-
-// const initialState = {};
-
-// const middleWare = [thunk];
-
-const store = configureStore({
-    reducer: {
-      auth: authReducer,
-      [authApi.reducerPath]: authApi.reducer
-    },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(authApi.middleware) 
-}); 
-
-export default store;
->>>>>>> 8ca482b (implement reduxjs toolkit)
