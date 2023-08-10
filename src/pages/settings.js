02e31bf382d758f0c7a48f1989018c19a2478dd5
cortejojicoy <jicoy.cortejo@gmail.com>
2023-08-10 14:09:34 +0800

    update settins on fetching data

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,72 +1,96 @@
 import { useState, useEffect } from 'react'
 import Head from 'next/head'
-import { useGetUserDetailsQuery } from '@/service/authService'
+import { useGetUserListQuery, useGetPermissionListQuery, useGetModuleListQuery } from '@/service/settingService'
 
 
 import AppLayout from '@/components/Layouts/AppLayout'
-import Table from '@/components/Table';
-import Form from '@/components/Form';
-import NavTab from '@/components/NavTab';
-import Card from '@/components/Card';
-import SkeletonScreen from '@/components/SkeletonScreen';
+import Table from '@/components/Table'
+import Form from '@/components/Form'
+import Card from '@/components/Card'
+import Error from '@/components/Error'
+import SkeletonScreen from '@/components/SkeletonScreen'
+import AlertError from '@/components/AlertError'
 
 const Setting = () => {
-    const columnCount = 3
-    const rowCount = 5
-    const { data, isLoading } = useGetUserDetailsQuery()
-    
-    const moduleId = "settings";
-    const menuGroup = "settings";
+    // const columnCount = 3
+    // const rowCount = 5
 
-    // const arrData = Object.entries(data)
+    const moduleId = "settings"
+    const menuGroup = "settings"
+
+    const [tableHeader, setTableHeader] = useState([])
+    const { data: userList, isLoading: userListLoading, isError: userErr, error, isSuccess: userSuccess } = useGetUserListQuery()
+    const { data: permission, isLoading: permissionListLoading, isError: permissionErr } = useGetPermissionListQuery()
+    const { data: moduleList} = useGetModuleListQuery()
     
 
-    const userData = [{
-        name: "John Jercai Cortejo",
-        email: "john@gmail.com",
-        roles: "Admin",
-    }]
+    // const isDataValid = !isError && isSuccess && data !== undefined && data !== null
+    // console.log(isDataValid)
+
+    const userData = userList?.userList ?? []
+    const permissionData = permission?.permission ?? []
+    const moduleData = moduleList?.moduleList ?? []
+
+    useEffect(() => {
+        if(userSuccess && Array.isArray(userData) && userData.length > 0) {
+            const headers = Object.keys(userData[0])
+            setTableHeader(headers)
+        }
+    }, [userSuccess, userData])
+
+    // if(isError) {
+    //     return (
+    //         <Error>
+    //             {error.data}
+    //         </Error>
+    //     )
+    // }
+
+    
+
+
+    // console.log(userData)
 
     const userRegistration = [
         {name: 'email', type: 'email', label: 'Email', placeholder: 'Enter email'},
         {name: 'password', type: 'password', label: 'Password', placeholder: 'Enter password'},
-        {
-            name: 'role',
-            type: 'dropdown',
-            label: 'Roles',
-            options: [
-                {value: 'admin', label: 'Admin'},
-                {value: 'user', label: 'User'}
-            ]
-        }
+        // {
+        //     name: 'role',
+        //     type: 'dropdown',
+        //     label: 'Roles',
+        //     options: [
+        //         {value: 'admin', label: 'Admin'},
+        //         {value: 'user', label: 'User'}
+        //     ]
+        // }
     ]
 
-    // console.log(data)
+    // console.log(moduleData)
 
     // Data for nav tabs
-    // const settings = [
-    //     {
-    //         label: 'User Account', 
-    //         content: <Form initialFields={userRegistration}/>, 
-    //         table:true,
-    //         tableTitle: "Users",
-    //         tableContent: <Table data={userData}/>
-    //     },
-    //     {
-    //         label: 'Employee', 
-    //         content: "Content for Tab 2 goes here",
-    //         table: false,
-    //         tableTitle: "",
-    //         tableContent: ""
-    //     },
-    //     {
-    //         label: 'System', 
-    //         content: "Content for Tab 3 goes here",
-    //         table: false,
-    //         tableTitle: "",
-    //         tableContent: ""
-    //     }
-    // ]
+    const permissionTab = [
+        {
+            label: 'Dashboard', 
+            content: "Content for Tab 1 goes here", 
+            table:true,
+            tableTitle: "Users",
+            tableContent: ""
+        },
+        {
+            label: 'Inventory', 
+            content: "Content for Tab 2 goes here",
+            table: false,
+            tableTitle: "",
+            tableContent: ""
+        },
+        {
+            label: 'Settings', 
+            content: "Content for Tab 3 goes here",
+            table: false,
+            tableTitle: "",
+            tableContent: ""
+        }
+    ]
 
     return (
         <AppLayout
@@ -82,11 +106,20 @@ const Setting = () => {
             </Head>
 
             <div className="p-8">
-                <Card title="User Authorization">
+                {/* <AlertError message={error}/> */}
+
+                <Card title="Create User">
                     <Form initialFields={userRegistration}/>
                 </Card>
 
-                <Table data={userData}/>
+                <Table 
+                    title="User List" 
+                    user={userData} 
+                    permission={permissionData} 
+                    module={moduleData} 
+                    tableHeader={tableHeader}
+                    isLoading={userListLoading}
+                    />
             </div>
 
         </AppLayout>
