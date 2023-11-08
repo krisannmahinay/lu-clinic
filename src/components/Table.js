86849c234d5650a5453ef15c6a139bdc61dfbffa
cortejojicoy <jicoy.cortejo@gmail.com>
2023-11-08 11:34:52 +0800

    refactor table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,7 @@
 
-import { useState, useRef, useEffect } from "react"
+import { useState, useRef, useEffect, forwardRef, useImperativeHandle } from "react"
 import SkeletonScreen from "./SkeletonScreen"
+import Select from 'react-select'
 import { useDispatch } from 'react-redux'
 import Modal from "./Modal"
 import Alert from "./Alert"
@@ -95,24 +96,80 @@ const dummyData = [
     {id:16, name: "Test Medicine7"},
 ]
 
-const Table = ({title, tableData, tableHeader, isLoading, permission, module, tab, onOpenModal, onSuccess, action, slug, children, disableTable}) => {
-    
+const bedGroup = [
+    {name: "General Ward", floor: "1st Floor", description: ""},
+]
+
+const bedType = [
+    {name: "Normal"},
+]
+
+const styleDropdown = {
+    control: (provided) => ({
+        ...provided,
+        // border: '1px solid gray',
+        padding: '0.1em',
+        boxShadow: 'none',
+        '&:hover': {
+          borderColor: 'gray',
+          border: '1px solid gray',
+        },
+      }),
+      input: (provided) => ({
+        ...provided,
+        inputOutline: 'none',
+      }),
+}
+
+const Table = forwardRef(({
+    title, 
+    tableData, 
+    tableHeader, 
+    isLoading, 
+    permission, 
+    module, 
+    tab, 
+    onOpenModal, 
+    onSuccess, 
+    action, 
+    slug, 
+    children, 
+    disableTable,
+    dynamicTable
+},ref) => {
     const imgRef = useRef(null)
     const dispatch = useDispatch()
     const [isModalOpen, setIsModalOpen] = useState(false)
     const [selectedRows, setSelectedRows] = useState([])
     const [openModalId, setOpenModalId] = useState("")
-
     const [imgLink, setImgLink] = useState("")
-    
     const [isZoomed, setIsZoomed] = useState(false)
     const [zoomScale, setZoomScale] = useState(1)
     const [maxHeight, setMaxHeight] = useState('70vh')
-
     const [alertType, setAlertType] = useState("")
     const [alertMessage, setAlertMessage] = useState([])
-
     const [inpatientAction, setInpatientAction] = useState("")
+
+    // state for dynamic table
+    const [formData, setFormData] = useState([])
+    const [editMode, setEditMode] = useState({})
+    const [idCounter, setIdCounter] = useState(0)
+    const [addRowClicked, setAddRowClicked] = useState(false)
+    const [selectedBedType, setSelectedBedType] = useState("")
+    const [selectedBedGroup, setSelectedBedGroup] = useState("")
+
+    useImperativeHandle(ref, () => ({
+        handleAddRow
+    }))
+
+    // console.log(tableData)
+
+    useEffect(() => {
+        if(tableData?.length > 0) {
+            setFormData(tableData)
+        }
+    }, [tableData])
+
 
     // useEffect(() => {
     //     if(alertType !== "") {
@@ -204,6 +261,117 @@ const Table = ({title, tableData, tableHeader, isLoading, permission, module, ta
         setImgLink(link)
     }
 
+    const handleSelectedBedType = (selectedOption) => {
+        setSelectedBedType(selectedOption)
+    }
+
+    const handleSelectedBedGroup = (selectedOption) => {
+        setSelectedBedGroup(selectedOption)
+    }
+
+    const handleFieldChange = (e, rowIndex, fieldName) => {
+        const updateData = formData.map((row, index) => {
+            if(index === rowIndex) {
+                return { ...row, [fieldName]: e.target.value }
+            }
+            return row
+        })
+        // console.log(updateData)
+        // setFormData(updateData)
+    }
+
+    const handleInputText = (a, b , c) => {
+        console.log(a,b,c)
+    }
+
+    const generateInitialFields = (tblHeader) => {
+        const initialFields = {}
+
+        tblHeader.forEach((header) => {
+            initialFields[header] = ''
+        })
+
+        return initialFields
+    }
+
+    const handleAddRow = () => {
+        const nextId = Math.max(0, ...tableData.map((data) => data.id)) + 1
+        const newRow = { 
+            id: nextId,
+            ...generateInitialFields(tableHeader) 
+        }
+
+        const formRow = {
+            id: nextId,
+            ...tableHeader.reduce((acc, fieldName) => {
+                acc[fieldName] = renderForm(fieldName, newRow, idCounter)
+                return acc
+            }, {})
+        }
+
+        setEditMode((prevModes) => ({
+            ...prevModes,
+            [newRow.id]: true,
+        }))
+        
+        setFormData((prev) => [...prev, formRow])
+        setIdCounter((prev) => prev + 1)
+    }
+
+    const toggleEditMode = (rowIndex) => {
+        setEditMode((prev) => ({
+          ...prev,
+            //   [rowId]: !prev[rowId],
+            [formData[rowIndex]?.id]: !prev[formData[rowIndex]?.id]
+        }))
+    }
+
+    const handleRemoveRow = (rowIndex, formRowId) => {
+        setFormData((prev) => 
+            prev.filter((_, index) => index !== rowIndex))
+    }
+
+    const renderForm = (fieldName, formRow, index) => {
+        console.log(index)
+        if(fieldName === 'bed_type') {
+            return (
+                <Select 
+                    options={bedType?.map(type => ({ value: type.name, label: type.name }))}
+                    onChange={handleSelectedBedType}
+                    isSearchable={true}
+                    isClearable={true}
+                    placeholder="Select..."
+                    classNamePrefix="react-select"
+                    styles={styleDropdown} 
+                    defaultValue={selectedBedType}
+                />
+            )
+        } else if(fieldName === 'bed_group') {
+            return (
+                <Select 
+                    options={bedGroup?.map(group => ({ value: group.name, label: `${group.name} - ${group.floor}` }))}
+                    onChange={handleSelectedBedGroup}
+                    isSearchable={true}
+                    isClearable={true}
+                    placeholder="Select..."
+                    classNamePrefix="react-select"
+                    styles={styleDropdown} 
+                    defaultValue={selectedBedGroup}
+                />
+            )
+        } else {
+            return (
+                <input
+                    type="text"
+                    name={`${fieldName}-${formRow.id}`}
+                    value={formRow[fieldName]}
+                    onChange={(e) => handleFieldChange(e, index, fieldName)}
+                    className="border border-gray-300 px-3 py-2 focus:border-gray-500 focus:outline-none"
+                />
+            )
+        }
+    }
+
     const renderContentBySlug = (action) => {
         switch(action) {
             case 'po':
@@ -260,8 +428,6 @@ const Table = ({title, tableData, tableHeader, isLoading, permission, module, ta
         }
     }
 
-    
-
     return (
         <>
             {alertMessage &&
@@ -272,11 +438,6 @@ const Table = ({title, tableData, tableHeader, isLoading, permission, module, ta
                     message={alertMessage} 
                 /> 
             }
-            
-            {/* <ModalTest
-            
-            moduleData={moduleData} 
-            /> */}
 
             <Modal 
                 // title={title}
@@ -292,7 +453,7 @@ const Table = ({title, tableData, tableHeader, isLoading, permission, module, ta
                 {renderContentBySlug(inpatientAction)}
             </Modal>
 
-            <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+            <div className="bg-white shadow overflow-y-auto scroll-custom sm:rounded-lg">
                 {isLoading ? (
                     <>
                         <SkeletonScreen rowCount={tableData.length} columnCount={tableHeader.length}/> 
@@ -301,6 +462,71 @@ const Table = ({title, tableData, tableHeader, isLoading, permission, module, ta
                     <>
                         {disableTable ? (
                             <div>{children}</div>
+                        ) : dynamicTable ? (
+                            <table className="min-w-full divide-y divide-gray-200">
+                                <thead>
+                                    <tr>
+                                        {tableHeader.map((tblHeader, tblHeaderIndex) => (
+                                            <th key={tblHeaderIndex} className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                {tblHeader}
+                                            </th>
+                                        ))}
+                                        <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Action
+                                        </th>
+                                    </tr>
+                                </thead>
+                                
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                    {formData.length === 0 ? (
+                                        <tr>
+                                            <td colSpan={tableHeader.length + 1} className="px-6 py-4 text-center">
+                                                No records found.
+                                            </td>
+                                        </tr>
+                                    ) : (
+                                        formData.map((formRow, index) => (
+                                            <tr key={formRow.id}>
+                                                {tableHeader.map((fieldName, fieldIndex) => (
+                                                    <td key={fieldIndex} className="px-6 py-4 whitespace-nowrap">
+                                                        {editMode[index] ? (
+                                                            renderForm(fieldName, formRow, index)
+                                                        ) : (
+                                                            formRow[fieldName]
+                                                        )}
+                                                    </td>
+                                                ))}
+                                                <td className="px-6 py-4 whitespace-nowrap">
+                                                    {editMode[formRow.id] ? (
+                                                        <button onClick={() => toggleEditMode(index)}>
+                                                            <svg fill="none" stroke="currentColor" className="h-5 w-5" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                                            </svg>
+                                                        </button>
+                                                    ) : (
+                                                        <button onClick={() => toggleEditMode(index)}>
+                                                            <svg fill="none" stroke="currentColor" className="h-5 w-5" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                                            </svg>
+                                                        </button>
+                                                    )}
+                                                    <button
+                                                        type="button"
+                                                        onClick={() => handleRemoveRow(index, formRow.id)}
+                                                        className="ml-2  text-[#cb4949] rounded-md px-2 py-1 focus:outline-none"
+                                                    >
+                                                        <svg fill="none" className="h-5 w-5" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                        </svg>
+                                                    </button>
+                                                </td>
+                                                
+                                            </tr>
+                                        ))
+                                    )}
+                                </tbody>
+
+                            </table>
                         ) : (
                             <table className="min-w-full divide-y divide-gray-200">
                                 <thead>
@@ -309,6 +535,7 @@ const Table = ({title, tableData, tableHeader, isLoading, permission, module, ta
                                             <th key={tblHeaderIndex} className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                 {tblHeader}
                                             </th>
+
                                         ))}
 
                                         {action && (
@@ -326,7 +553,7 @@ const Table = ({title, tableData, tableHeader, isLoading, permission, module, ta
                                                 No records found.
                                             </td>
                                         </tr>
-                                    ): (
+                                    ) : (
                                         tableData.map((tblBody, tblBodyIndex) => (
                                             <tr key={tblBodyIndex}>
                                                 {tableHeader.map((tblHeader) => (
@@ -423,6 +650,6 @@ const Table = ({title, tableData, tableHeader, isLoading, permission, module, ta
 
         </>
     )
-}
+})
 
 export default Table
