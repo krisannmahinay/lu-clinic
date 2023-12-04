61b92d30a716f369c53500ab712b811378af2b25
cortejojicoy <jicoy.cortejo@gmail.com>
2023-12-04 10:29:26 +0800

    update dropdown

--- src/components/DropdownLink.js
+++ src/components/DropdownLink.js
@@ -46,7 +46,7 @@ export const DropdownNotification = ({ children, ...props }) => (
 export const DropdownExport = ({ children, ...props }) => (
     <Menu.Item>
         {({ active }) => (
-            <button className={`w-full text-left block px-4 py-2 text-sm leading-5 text-gray-700 ${
+            <button className={`w-full text-left block px-4 py-1 font-medium text-xs leading-5 text-gray-500 ${
                 active ? 'bg-gray-100' : ''
             } focus:outline-none transition duration-150 ease-in-out`}
             {...props}>
