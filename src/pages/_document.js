29f57fdaac16b2c3a038d425197926417f33a909
cortejojicoy <jicoy.cortejo@gmail.com>
2023-12-23 13:15:26 +0800

    update _documents

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
                 <Head>
                     <link rel="icon" href="https://i.imgur.com/OyjAuyh.png"/>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap" rel="stylesheet" />          
                 </Head>
 
