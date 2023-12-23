2f820e9e678c8f815a7d95c048f9050794556c8e
cortejojicoy <jicoy.cortejo@gmail.com>
2023-12-23 15:07:20 +0800

    update next config

--- next.config.js
+++ next.config.js
@@ -16,43 +16,43 @@ const nextConfig = {
         API_URL: 'http://localhost/api',
         NEXT_PUBLIC_PUSHER_APP_KEY: '1bfa0e4b900a03079829',
         NEXT_PUBLIC_PUSHER_APP_CLUSTER: 'ap1',
-        NEXT_PUBLIC_WS_HOST: 'localhost:80',
+        NEXT_PUBLIC_WS_HOST: 'localhost',
         NEXT_PUBLIC_WS_PORT: 6001,
         NEXT_PUBLIC_WSS_PORT: 443
     },
     publicRuntimeConfig: {
         pwa: {
-            // manifest: "/manifest.json", 
-            manifest: {
-                "theme_color": "#fff",
-                    "background_color": "#fff",
-                    "display": "standalone",
-                    "start_url": "/",
-                    "name": "HMS",
-                    "short_name": "hms",
-                    "icons": [
-                        {
-                            "src": "/public/icon-192x192.png",
-                            "sizes": "192x192",
-                            "type": "image/png"
-                        },
-                        {
-                            "src": "/public/icon-256x256.png",
-                            "sizes": "256x256",
-                            "type": "image/png"
-                        },
-                        {
-                            "src": "/public/icon-384x384.png",
-                            "sizes": "384x384",
-                            "type": "image/png"
-                        },
-                        {
-                            "src": "/public/icon-512x512.png",
-                            "sizes": "512x512",
-                            "type": "image/png"
-                        }
-                    ]
-            }
+            manifest: "/manifest.json", 
+            // manifest: {
+            //     "theme_color": "#fff",
+            //         "background_color": "#fff",
+            //         "display": "standalone",
+            //         "start_url": "/",
+            //         "name": "HMS",
+            //         "short_name": "hms",
+            //         "icons": [
+            //             {
+            //                 "src": "/public/icon-192x192.png",
+            //                 "sizes": "192x192",
+            //                 "type": "image/png"
+            //             },
+            //             {
+            //                 "src": "/public/icon-256x256.png",
+            //                 "sizes": "256x256",
+            //                 "type": "image/png"
+            //             },
+            //             {
+            //                 "src": "/public/icon-384x384.png",
+            //                 "sizes": "384x384",
+            //                 "type": "image/png"
+            //             },
+            //             {
+            //                 "src": "/public/icon-512x512.png",
+            //                 "sizes": "512x512",
+            //                 "type": "image/png"
+            //             }
+            //         ]
+            // }
         },
     },
 }
