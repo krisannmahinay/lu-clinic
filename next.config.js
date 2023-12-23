17417dad122286ac69e9099e50a37650cb62a322
cortejojicoy <jicoy.cortejo@gmail.com>
2023-12-23 12:38:03 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -22,7 +22,37 @@ const nextConfig = {
     },
     publicRuntimeConfig: {
         pwa: {
-            manifest: "/manifest.json", 
+            // manifest: "/manifest.json", 
+            manifest: {
+                "theme_color": "#fff",
+                    "background_color": "#fff",
+                    "display": "standalone",
+                    "start_url": "/",
+                    "name": "HMS",
+                    "short_name": "hms",
+                    "icons": [
+                        {
+                            "src": "/public/icon-192x192.png",
+                            "sizes": "192x192",
+                            "type": "image/png"
+                        },
+                        {
+                            "src": "/public/icon-256x256.png",
+                            "sizes": "256x256",
+                            "type": "image/png"
+                        },
+                        {
+                            "src": "/public/icon-384x384.png",
+                            "sizes": "384x384",
+                            "type": "image/png"
+                        },
+                        {
+                            "src": "/public/icon-512x512.png",
+                            "sizes": "512x512",
+                            "type": "image/png"
+                        }
+                    ]
+            }
         },
     },
 }
