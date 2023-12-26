96a88723c250477a44f4d5eb9b3d3d40ee273a5f
cortejojicoy <jicoy.cortejo@gmail.com>
2023-12-26 10:53:26 +0800

    update return 404 pages

--- src/components/SystemError.js
+++ src/components/SystemError.js
@@ -10,14 +10,11 @@ const SystemError = ({message}) => {
 
     return (
         <div className="flex items-center justify-center h-screen bg-gray-100">
-            <div className="max-w-md px-6 py-8 bg-white rounded-lg shadow-lg">
+            <div>
                 <h1 className="text-2xl font-bold text-gray-800">401 Unauthorized</h1>
                 <p className="mt-4 text-gray-600">
                 Oops! You are not authorized to access this page.
                 </p>
-                <button onClick={handleClick} className="mt-6 px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600">
-                    Go Back to Login
-                </button>
             </div>
         </div>
     )
