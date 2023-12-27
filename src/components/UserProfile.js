ccb420a130b893f4983a9be081631f8c53a5ac69
cortejojicoy <jicoy.cortejo@gmail.com>
2023-12-27 17:34:56 +0800

    update user profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -18,10 +18,11 @@ const UserProfile = ({data, type, module, permission}) => {
     }
 
     const handleAutoSave = debounce((moduleId) => {
-        console.log(moduleId)
+        const toggledModule = modules?.module.find(module => module.module_id === moduleId)
+        
     })
 
-    // console.log(modules)
+    console.log(modules)
 
     return (
         <div>
@@ -73,7 +74,7 @@ const UserProfile = ({data, type, module, permission}) => {
                                                 className="sr-only peer"
                                             />
                                             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-300 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                            <span className="ms-3 text-sm font-medium text-gray-900">{mod.name}</span>
+                                            <span className="ms-3 text-sm font-medium text-gray-700">{mod.type === 'sub' ? `--- ${mod.name}` : mod.name}</span>
                                         </label>
                                     </div>
                                 ))}
