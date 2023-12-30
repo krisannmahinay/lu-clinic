0adad5e28b8f2c0465ffa4c8809357cce00520f8
cortejojicoy <jicoy.cortejo@gmail.com>
2023-12-30 22:30:14 +0800

    update configuration

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const withPWA = require('next-pwa')({
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        NEXT_SOCKET_IO: 'http://localhost:6001',
+        NEXT_SOCKET_IO: 'http://127.0.0.1:6001',
         NEXT_ENV: 'development',
         API_URL: 'http://localhost/api',
         // NEXT_PUBLIC_PUSHER_APP_KEY: '1bfa0e4b900a03079829',
