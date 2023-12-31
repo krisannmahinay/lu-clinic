f104470a597a4bf60981c46558bbf5b887daad46
cortejojicoy <jicoy.cortejo@gmail.com>
2023-12-31 14:46:58 +0800

    update form utility

--- src/utils/forms.js
+++ src/utils/forms.js
@@ -14,6 +14,76 @@ export const userRegistration = [
     }
 ]
 
-export const opd = [
-    
-]
+export const generateOpdForms = (physicianList) => {
+    const phyisicianOptions = physicianList?.map(ph => ({
+        value: ph.user_id,
+        label: `Dr. ${ph.identity?.first_name} ${ph.identity?.last_name}`,
+    }))
+
+    return [
+        {name: 'last_name', type: 'text', label: 'Last Name', placeholder: 'Type...'},
+        {name: 'first_name', type: 'text', label: 'Given Name', placeholder: 'Type...'},
+        {name: 'middle_name', type: 'text', label: 'Middle Name', placeholder: 'Type...'},
+        {name: 'birth_date', type: 'date', label: 'Birthdate'},
+        {name: 'age', type: 'text', label: 'Age', disabled: true},
+        {
+            name: 'gender',
+            type: 'dropdown',
+            label: 'Gender',
+            options: [
+                {value: 'male', label: 'Male'},
+                {value: 'female', label: 'Female'}
+            ]
+        },
+        {
+            name: 'admiting_physician',
+            type: 'dropdown',
+            label: 'Physician',
+            options: phyisicianOptions
+        },
+        {name: 'standard_charge', type: 'text', label: 'Standard Charge', disabled: true}
+    ]
+}
+
+export const generateIpdForms = (physicianList, activeBedList) => {
+    const physicOptions = physicianList?.map(ph => ({
+        value: ph.user_id,
+        label: `Dr. ${ph.identity?.first_name} ${ph.identity?.last_name}`,
+    }))
+
+    const bedOptions = activeBedList?.map(bed => ({
+        value: bed.id,
+        label: `${bed.name} | ${bed.bed_group?.name} - ${bed.bed_group?.bed_floor?.floor}`
+    })) 
+
+    return [
+        {name: 'last_name', type: 'text', label: 'Last Name', placeholder: 'Type...'},
+        {name: 'first_name', type: 'text', label: 'Given Name', placeholder: 'Type...'},
+        {name: 'middle_name', type: 'text', label: 'Middle Name', placeholder: 'Type...'},
+        {
+            name: 'bed', 
+            type: 'dropdown', 
+            label: 'Bed', 
+            options: bedOptions, 
+        },
+        {name: 'birth_date', type: 'date', label: 'Birthdate'},
+        {name: 'age', type: 'text', label: 'Age', disabled: true},
+        {
+            name: 'gender',
+            type: 'dropdown',
+            label: 'Gender',
+            options: [
+                {value: 'male', label: 'Male'},
+                {value: 'female', label: 'Female'}
+            ]
+        },
+        {
+            name: 'admiting_physician',
+            type: 'dropdown',
+            label: 'Physician',
+            options: physicOptions
+        },
+        {name: 'standard_charge', type: 'text', label: 'Standard Charge', disabled: true}
+    ]
+}
+
