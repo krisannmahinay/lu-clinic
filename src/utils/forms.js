1e7dd8f2cb3fdadbe858af62bc3216b8654b432f
cortejojicoy <jicoy.cortejo@gmail.com>
2023-12-31 20:54:10 +0800

    update forms for personal information

--- src/utils/forms.js
+++ src/utils/forms.js
@@ -25,7 +25,7 @@ export const generateOpdForms = (physicianList) => {
         {name: 'first_name', type: 'text', label: 'Given Name', placeholder: 'Type...'},
         {name: 'middle_name', type: 'text', label: 'Middle Name', placeholder: 'Type...'},
         {name: 'birth_date', type: 'date', label: 'Birthdate'},
-        {name: 'age', type: 'text', label: 'Age', disabled: true},
+        {name: 'age', type: 'text', value: null, label: 'Age', disabled: true},
         {
             name: 'gender',
             type: 'dropdown',
@@ -41,7 +41,7 @@ export const generateOpdForms = (physicianList) => {
             label: 'Physician',
             options: phyisicianOptions
         },
-        {name: 'standard_charge', type: 'text', label: 'Standard Charge', disabled: true}
+        {name: 'standard_charge', type: 'text', value: null, label: 'Standard Charge', disabled: true}
     ]
 }
 
@@ -84,6 +84,80 @@ export const generateIpdForms = (physicianList, activeBedList) => {
             options: physicOptions
         },
         {name: 'standard_charge', type: 'text', label: 'Standard Charge', disabled: true}
+    ]
+}
+
+export const generateInfoForms = (formData, province, municipality, barangay) => {
+    console.log(formData)
+    const provOptions = province?.map(data => ({
+        label: data.name,
+        value: data.code
+    })) 
+
+    const brgyOptions = barangay?.map(data => ({
+        label: data.name,
+        value: data.code
+    })) 
+
+    const municipalOptions = municipality?.map(data => ({
+        label: data.name,
+        value: data.code
+    })) 
+
+    return [
+        {name: 'last_name', type: 'text', value: formData.last_name, label: 'Last Name', placeholder: 'Type...'},
+        {name: 'first_name', type: 'text', value: formData.first_name, label: 'Given Name', placeholder: 'Type...'},
+        {name: 'middle_name', type: 'text', value: formData.middle_name, label: 'Middle Name', placeholder: 'Type...'},
+        {name: 'email', type: 'email', value: formData.email, label: 'Email', placeholder: 'Type...'},
+        {name: 'birth_date', type: 'date', value: formData.email, label: 'Birthdate'},
+        {name: 'birth_place', type: 'text', value: formData.birth_place, label: 'Birth Place', placeholder: 'Type...'},
+        {
+            name: 'gender',
+            type: 'dropdown',
+            label: 'Gender',
+            value: formData.gender,
+            options: [
+                {value: 'male', label: 'Male'},
+                {value: 'female', label: 'Female'}
+            ]
+        },
+        {
+            name: 'civil_status',
+            type: 'dropdown',
+            label: 'Civil Status',
+            value: formData.civil_status,
+            options: [
+                {value: "s", label: "Single"},
+                {value: "m", label: "Married"},
+                {value: "sep", label: "Separated"},
+                {value: "w", label: "Widow"}
+            ]
+        },
+        {name: 'contact_no', type: 'number', label: 'Contact No', value:formData.contact_no},
+        {name: 'age', type: 'text', label: 'Age', disabled: true},
+        {
+            name: 'province',
+            type: 'dropdown',
+            label: 'Province',
+            value: formData.province,
+            options: provOptions
+        },
+        {
+            name: 'municipality',
+            type: 'dropdown',
+            label: 'Municipality',
+            value: formData.municipality,
+            options: municipalOptions
+        },
+        {
+            name: 'barangay',
+            type: 'dropdown',
+            label: 'Barangay',
+            value: formData.barangay,
+            options: brgyOptions
+        },
+        {name: 'street', type: 'text', value: formData.street, label: 'Street', placeholder: 'Type...'},
+        {name: 'no_blk_lot', type: 'text', value: formData.no_blk_lot, label: 'No/Blk/Lot', placeholder: 'Type...'},
     ]
 }
 
