be064ec50e9c2a47db3cd2372959dd07f87a5816
cortejojicoy <jicoy.cortejo@gmail.com>
2023-12-31 14:24:44 +0800

    ad utility for multitple forms

---
+++ src/utils/forms.js
@@ -0,0 +1,19 @@
+
+export const userRegistration = [
+    {name: 'email', type: 'email', label: 'Email', placeholder: 'Enter email'},
+    {name: 'password', type: 'password', label: 'Password', placeholder: 'Enter password'},
+    {
+        name: 'roles',
+        type: 'dropdown',
+        label: 'Roles',
+        options: [
+            {value: 'admin', label: 'Admin'},
+            {value: 'nurse', label: 'Nurse'},
+            {value: 'doctor', label: 'Doctor'}
+        ]
+    }
+]
+
+export const opd = [
+    
+]
