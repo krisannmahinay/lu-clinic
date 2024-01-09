8376b43081c4eab4a0ed5326a60fb1d4c2e53396
cortejojicoy <jicoy.cortejo@gmail.com>
2024-01-09 08:43:54 +0800

    update skeleton screen bar loading

--- src/components/SkeletonScreen.js
+++ src/components/SkeletonScreen.js
@@ -1,28 +1,57 @@
 
 
-const SkeletonScreen = () => {
+const SkeletonScreen = ({loadingType}) => {
     return (
-        <div className="grid px-7 gap-y-2 pt-[7rem]">
-            <div className="flex justify-between">
+        loadingType === 'table' ? (
+            <div className="grid px-7 gap-y-2 pt-[7rem]">
+                <div className="flex justify-between">
+                    <div className="flex space-x-3">
+                        <div className="w-28 h-8 bg-gray-300 rounded animate-pulse"></div>
+                        <div className="w-28 h-8 bg-gray-300 rounded animate-pulse"></div>
+                    </div>
+
+                    <div>
+                        <div className="w-40 h-8 bg-gray-300 rounded animate-pulse"></div>
+                    </div>
+                </div>
                 <div className="flex space-x-3">
-                    <div className="w-28 h-8 bg-gray-300 rounded animate-pulse"></div>
-                    <div className="w-28 h-8 bg-gray-300 rounded animate-pulse"></div>
-                </div>
+                    <div className="w-1/2 h-8 bg-gray-300 rounded animate-pulse"></div>
+                    <div className="w-1/2 h-8 bg-gray-300 rounded animate-pulse"></div>
+                    <div className="w-1/2 h-8 bg-gray-300 rounded animate-pulse"></div>
+                    <div className="w-1/2 h-8 bg-gray-300 rounded animate-pulse"></div>
 
-                <div>
-                    <div className="w-40 h-8 bg-gray-300 rounded animate-pulse"></div>
                 </div>
+                <div className="w-full h-8 bg-gray-300 rounded animate-pulse"></div>
+                <div className="w-full h-8 bg-gray-300 rounded animate-pulse"></div>
             </div>
-            <div className="flex space-x-3">
-                <div className="w-1/2 h-8 bg-gray-300 rounded animate-pulse"></div>
-                <div className="w-1/2 h-8 bg-gray-300 rounded animate-pulse"></div>
-                <div className="w-1/2 h-8 bg-gray-300 rounded animate-pulse"></div>
-                <div className="w-1/2 h-8 bg-gray-300 rounded animate-pulse"></div>
+        ) : loadingType === 'mainPatientModule' ? (
+            <div className="grid px-7 gap-y-2 pt-[7rem]">
+                <div className="flex space-x-3">
+                    <div className="w-full h-40 bg-gray-300 rounded animate-pulse"></div>
+                    <div className="w-full h-40 bg-gray-300 rounded animate-pulse"></div>
+                    <div className="w-full h-40 bg-gray-300 rounded animate-pulse"></div>
+                </div>
+                <div className="flex justify-between">
+                    <div className="flex space-x-3">
+                        <div className="w-28 h-8 bg-gray-300 rounded animate-pulse"></div>
+                        <div className="w-28 h-8 bg-gray-300 rounded animate-pulse"></div>
+                    </div>
 
+                    <div>
+                        <div className="w-40 h-8 bg-gray-300 rounded animate-pulse"></div>
+                    </div>
+                </div>
+                <div className="flex space-x-3">
+                    <div className="w-1/2 h-8 bg-gray-300 rounded animate-pulse"></div>
+                    <div className="w-1/2 h-8 bg-gray-300 rounded animate-pulse"></div>
+                    <div className="w-1/2 h-8 bg-gray-300 rounded animate-pulse"></div>
+                    <div className="w-1/2 h-8 bg-gray-300 rounded animate-pulse"></div>
+
+                </div>
+                <div className="w-full h-8 bg-gray-300 rounded animate-pulse"></div>
+                <div className="w-full h-8 bg-gray-300 rounded animate-pulse"></div>
             </div>
-            <div className="w-full h-8 bg-gray-300 rounded animate-pulse"></div>
-            <div className="w-full h-8 bg-gray-300 rounded animate-pulse"></div>
-        </div>
+        ) : ''
     )
 }
 
