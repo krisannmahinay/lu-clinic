76306925288634e11ca7de8f86a61c48ed021d48
cortejojicoy <jicoy.cortejo@gmail.com>
2024-01-20 20:46:34 +0800

    update context

--- src/utils/context.js
+++ src/utils/context.js
@@ -4,17 +4,35 @@ import { createContext, useContext } from "react"
 export const FormContext = createContext({
     // PatientInformation component
     data: null,
+    provinceData: null,
+    municipalityData: null,
+    barangayData: null,
     initialFields: [],
     enableAutoSave: false,
     onFormChange: () => {},
+    onSelectProvince: () => {},
 
+    // Add OPD Form Context
+    ref: null,
+    // data: null,
+    // enableAutoSave: false,
+    enableAddRow: false,
+    onSuccess: () => {},
+    onLoading: () => {},
+    onCloseSlider: () => {},
+    onSetAlertType: () => {},
+    onSetAlertMessage: () => {}
+})
+
+export const TableContext = createContext({
     // Table component
     tableHeader: null,
     tableData: null,
     isLoading: false,
     onChecked: () => {},
     onClick: () => {},
-    onEdit: () => {}
+    onEdit: () => {},
 })
 
 export const useFormContext = () => useContext(FormContext)
+export const useTableContext = () => useContext(TableContext)
