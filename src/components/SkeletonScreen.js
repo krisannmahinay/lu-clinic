63b9fa413868fef8a4334ae8b44f2e0a8df5e588
cortejojicoy <jicoy.cortejo@gmail.com>
2024-02-06 13:42:06 +0800

    update skeleton screen

--- src/components/SkeletonScreen.js
+++ src/components/SkeletonScreen.js
@@ -4,7 +4,7 @@ import React from "react"
 const SkeletonScreen = ({loadingType}) => {
     return (
         loadingType === 'table' ? (
-            <div className="grid px-7 gap-y-2 pt-[7rem]">
+            <div className="grid px-7 gap-y-2 pr-[6rem] pl-[6rem] pt-[7rem]">
                 <div className="flex justify-between">
                     <div className="flex space-x-3">
                         <div className="w-28 h-8 bg-gray-300 rounded animate-pulse"></div>
